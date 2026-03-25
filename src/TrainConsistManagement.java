9d228a9082803381e6d7419957d8941a65a91a64
PriyanshiSri05 <priyanshi.sri603@gmail.com>
2026-03-25 08:42:52 +0530

    Base application setup

--- src/TrainConsistManagement.java
+++ src/TrainConsistManagement.java
@@ -1,4 +1,16 @@
-package PACKAGE_NAME;
+import java.util.ArrayList;
+import java.util.List;
 
 public class TrainConsistManagement {
+
+    public static void main(String[] args) {
+
+        System.out.println("====================================");
+        System.out.println("=== Train Consist Management App ===");
+        System.out.println("====================================");
+        List<String> bogies = new ArrayList<>();
+        System.out.println("Train consist initialized.");
+        System.out.println("Initial number of bogies: " + bogies.size());
+
+    }
 }
