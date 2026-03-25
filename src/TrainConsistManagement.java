02e4df9f02da35f373c3fee43d8f810cd75532c2
ramsaiangadala3107-arch <ramsaiangadala3107@gmail.com>
2026-03-25 09:05:56 +0530

    UC3:Hardcoded palindrome

--- src/TrainConsistManagement.java
+++ src/TrainConsistManagement.java
@@ -1,24 +1,18 @@
-import java.util.ArrayList;
+import java.util.HashSet;
 
 public class TrainConsistManagement {
     public static void main(String[] args) {
 
         System.out.println("=== Train Consist Management App ===");
 
-        ArrayList<String> passengerBogies = new ArrayList<>();
+        HashSet<String> bogieIds = new HashSet<>();
 
-        passengerBogies.add("Sleeper");
-        passengerBogies.add("AC Chair");
-        passengerBogies.add("First Class");
+        bogieIds.add("BG101");
+        bogieIds.add("BG102");
+        bogieIds.add("BG103");
+        bogieIds.add("BG101");
+        bogieIds.add("BG102");
 
-        System.out.println("Passenger Bogies after addition: " + passengerBogies);
-
-        passengerBogies.remove("AC Chair");
-
-        System.out.println("After removing AC Chair: " + passengerBogies);
-
-        System.out.println("Is Sleeper present? " + passengerBogies.contains("Sleeper"));
-
-        System.out.println("Final Bogie List: " + passengerBogies);
+        System.out.println("Unique Bogie IDs: " + bogieIds);
     }
 }
